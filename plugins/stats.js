68e1be23839276b0d46936facb3c932592967224
Jonn3y <jonas.engelhardt@live.de>
2016-01-16 15:21:50 +0100

    Update stats.js
    added "PlexPy" Link

--- plugins/stats.js
+++ plugins/stats.js
@@ -32,5 +32,33 @@ stats = {
             catch(e) {
             }
         }
+
+        var nav_bar_right = document.body.getElementsByClassName("nav-bar-right")[0];
+
+        var list_element = document.createElement("li");
+
+        var stats_link = document.createElement("a");
+        stats_link.setAttribute("id", "stats-page-link");
+        stats_link.setAttribute("title", "Transmogrify for Plex stats");
+        stats_link.setAttribute("href", settings["plex_server_uri"]);
+        stats_link.setAttribute("target", "_blank");
+
+        var stats_glyph = document.createElement("i");
+        stats_glyph.setAttribute("class", "glyphicon stats");
+
+        stats_link.appendChild(stats_glyph);
+        list_element.appendChild(stats_link);
+
+        nav_bar_right.insertBefore(list_element, nav_bar_right.firstChild);
+
+        // event handler for firefox to open stats page
+        var is_firefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+        if (is_firefox) {
+            try {
+                stats_link.addEventListener("click", utils.openStatsPage, false);
+            }
+            catch(e) {
+            }
+        }
     }
 }
